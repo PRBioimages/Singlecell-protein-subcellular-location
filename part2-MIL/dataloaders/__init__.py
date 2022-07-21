b801c537aa86e2b095fef62bcbde8297939ab0f9
Pattern Recognition and Bioimage Informatics Group <84379335+PRBioimages@users.noreply.github.com>
2022-07-21 10:29:23 +0800

    Delete part2-MIL directory

--- part2-MIL/dataloaders/__init__.py
+++
@@ -1,6 +0,0 @@
-from dataloaders.experiments import *
-from configs import Config
-
-
-def get_dataloader(cfg: Config):
-    return globals().get(cfg.experiment.name)
--- part2-MIL/losses/__init__.py
+++
@@ -1,8 +0,0 @@
-from losses.regular import ce, label_smooth_ce, label_smooth_ce_ohem, mse, mae, bce, sl1, bce_mse
-from losses.regular import focal_loss, bce_ohem, criterion_margin_focal_binary_cross_entropy, ce_oheb, bi_tempered_loss, \
-    resampleloss
-
-
-def get_loss(cfg):
-    return globals().get(cfg.loss.name)(**cfg.loss.param)
-
